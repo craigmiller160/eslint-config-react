c0f1d7ee7d7bcd8ec990f6157d4eab9c0f92c2d3
Craig <craigmiller160@gmail.com>
2021-12-29 15:26:57 -0500

    Added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    parser: '@babel/eslint-parser',
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    extends: [
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended'
+    ]
+};
